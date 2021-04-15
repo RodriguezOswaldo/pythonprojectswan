59e9ba0917125565bbb7d7beb0f518f4918b791d
OwnOS <Owner2303@gmail.com>
2021-04-15 16:41:04 -0400

    rock, paper, scissors

---
+++ ro-pa-sci/file.py
@@ -0,0 +1,17 @@
+import random
+
+def play():
+    user = input("'r' for rock, 'p' for paper, 's' for scissors \n" )
+    computer = random.choice(['r', 'p', 's'])
+
+    print(f'user chose {user}')
+    print(f'computer chose  {computer}')
+    if user == computer:
+     print('tie')
+
+    if is_win(user, computer):
+        return 'You win!'
+      
+   
+
+play()
